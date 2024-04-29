c8cf9e94fa19fbc62f8b8d14ff524b17ff095d33
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-29 08:40:28 -0700

    comment out second url path

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -3,5 +3,5 @@ from .views import MovieListAPIView
 
 urlpatterns = [
     path('', MovieListAPIView.as_view(), name='movie-list'),
-    path('fetch-movies/', MovieListAPIView.as_view(), name='fetch-movies'),
+    # path('fetch-movies/', MovieListAPIView.as_view(), name='fetch-movies'),
 ]
