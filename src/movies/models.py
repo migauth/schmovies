9f49c1b79b7e63e53bf3fa1fc03bac1474a100ba
Michael Gauthier <mikegauthier222@hotmail.com>
2024-05-01 08:05:21 -0700

    change release_year value type to string in database

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    genre = models.CharField(max_length=100)
-    release_year = models.IntegerField()
-    poster_url = models.URLField()
+    title = models.CharField(max_length=255, default="Unknown Title")
+    description = models.TextField(default="No description available.")
+    genre = models.CharField(max_length=100, default="Unknown Genre")
+    release_year = models.CharField(max_length=4, default='none')
+    poster_url = models.URLField(default="https://example.com/default_poster.jpg")
 
     def __str__(self):
         return self.title
+
