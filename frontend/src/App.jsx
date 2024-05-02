bd766c2d93e000811982712b9275356606eed923
Charlisteketee <charlisteketee@gmail.com>
2024-05-02 10:51:47 -0700

    Add About, login button, and logo to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
+import Navbar from './components/Navbar';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img className='logo' src="/schmovies.png" alt="schmovies-logo" />
-            </header>
+            
             <main>
+                <Navbar />
                 <MovieList />
             </main>
         </div>
