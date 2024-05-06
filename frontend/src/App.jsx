ac21bffd50b01f5e4a4c40cc4796c8faf3bc7828
Michael Gauthier <mikegauthier222@hotmail.com>
2024-05-06 14:06:10 -0700

    add about and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import MovieList from "./components/MovieList";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("movieList"); // State to control which page to display
+
+  const pages = {
+    movieList: <MovieList />,
+    about: <About />,
+    home: <MovieList/>
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page); // Set currentPage to the selected page
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar handlePageChange={handlePageChange} />
       <main>
-        <MovieList />
+        {pages[currentPage]} {/* Render the component based on the currentPage */}
       </main>
     </div>
   );
