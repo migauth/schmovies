eccfc984e1550bbd006dc3f1c82ea552f4058d5f
Michael Gauthier <mikegauthier222@hotmail.com>
2024-05-10 09:15:10 -0700

    add openai

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -4,16 +4,25 @@ import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import UserPreferences
+import openai
+from openai import OpenAI
 
-AI_MODEL_ENDPOINT = '' #API Model endpoint url should go here
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=OPENAI_API_KEY,
+)
 
 @csrf_exempt
 def submit_quiz(request):
     if request.method == 'POST':
+        
         # Extract user's preferences from the request
         data = json.loads(request.body)
         genre = data.get('searchText')
+        
         # submitted_answers = json.loads(request.body) this is probably a better name for the data variable
 
         # Call the function to get movie suggestions based on user's preference
@@ -29,11 +38,27 @@ def submit_quiz(request):
     # If the request method is not POST, return an error
     return JsonResponse({'error': 'Method not allowed'}, status=405)
 
-def get_movie_suggestions(genre):
+def get_movie_suggestions(genre,keywords):
+    
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": f'reduce this down to one word that sums the whole thing up: {keywords}',
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+    keyword = chat_completion.choices[0].message.content
+
+    print(keyword)
+    
     # Make request to TMDb API to fetch movie suggestions based on genre
     api_key = TMDB_API_KEY
-    url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language=en-US&sort_by=popularity.desc&with_genres={genre}'
+    url2 = f'https://api.themoviedb.org/3/discover/movie?api_key=e010f27028932aac6c5b0fcdfcfc359f&language=en-US&sort_by=popularity.desc&with_genres=28&query={keyword}'
     
+    url = f'https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&with_genres={genre}&query={keyword}'
+
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
