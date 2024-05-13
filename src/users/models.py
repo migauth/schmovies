67f6d303cd6e8f09280de0186b3fb0dea1ae971b
Luiza Leao <luiza.leao96@gmail.com>
2024-05-13 17:00:59 -0400

    Switch to default user model

--- src/users/models.py
+++ src/users/models.py
@@ -1,14 +1,1 @@
-from django.db import models
-
-
 # Create your models here.
-
-class User(models.Model):
-    username = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.username
