2eeb8bf1fb79a598ee23ab88c176212f007a0264
Luiza Leao <luiza.leao96@gmail.com>
2024-05-13 19:05:03 -0400

    Create templates

--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to login page after successful registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def user_logout(request):
     logout(request)
