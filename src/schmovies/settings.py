4661ef4a95afb9e1208a205e5bb7912490d41c26
Luiza Leao <luiza.leao96@gmail.com>
2024-05-14 21:15:50 -0400

    Add static files setup

--- src/schmovies/settings.py
+++ src/schmovies/settings.py
@@ -141,7 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Path to your static files directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -149,6 +153,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
