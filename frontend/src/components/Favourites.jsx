3342a98188d49eb2b3b5356166d30f588027aa77
Charlisteketee <charlisteketee@gmail.com>
2024-05-15 17:16:59 -0700

    Favourites show up on favourites page

--- frontend/src/components/Favourites.jsx
+++ frontend/src/components/Favourites.jsx
@@ -8,6 +8,7 @@ const Favourites = ({ favouriteMovies }) => {
         {favouriteMovies.map(movie => (
           <div key={movie.id}>
             <h3>{movie.title}</h3>
+            <img src={movie.poster_url} alt={movie.title} className="movie-popup__image" />
             <p>Genre: {movie.genre}</p>
             <p>Release Year: {movie.release_year}</p>
           </div>
