c6c57adb0c5f20d9e34d5e30a64337f6d65db4d1
Charlisteketee <charlisteketee@gmail.com>
2024-05-16 16:40:49 -0700

    Popout feature for fav movies

--- frontend/src/components/Favourites.jsx
+++ frontend/src/components/Favourites.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import "../styles/Favourites.scss"; // Import the SCSS file
+import MoviePopup from './MoviePopup';
 
-const Favourites = ({ favouriteMovies, handleMovieClick }) => {
+const Favourites = ({ favouriteMovies, selectedMovie, closePopup, handleMovieClick }) => {
 
     console.log("favouriteMovies:", favouriteMovies);
     return (
@@ -11,16 +12,27 @@ const Favourites = ({ favouriteMovies, handleMovieClick }) => {
         </div>
         <div className="favourites-list">
           {favouriteMovies.map(movie => (
-            <div key={movie.id}
+            <div
+              key={movie.id}
               className="favourites-list-item"
               onClick={() => handleMovieClick(movie)}
-              >
+            >
               <h3>{movie.title}</h3>
               <img src={movie.poster_url} alt={movie.title} className="favourites-image" />
               <p>Genre: {movie.genre}</p>
               <p>Release Year: {movie.release_year}</p>
             </div>
           ))}
+                          {/* Render the backdrop and the pop-up if a movie is selected */}
+                          {selectedMovie && (
+                    <>
+                        <div className="backdrop" onClick={closePopup}></div>
+                        <MoviePopup
+                            movie={selectedMovie}
+                            onClose={closePopup} // Pass the close function to the pop-up
+                        />
+                    </>
+                )}
         </div>
       </div>
     );
