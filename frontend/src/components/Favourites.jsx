97dd696f70813d721fc1ee77bb72267e9271b336
Charlisteketee <charlisteketee@gmail.com>
2024-05-16 13:41:40 -0700

    Trying to fix popup not opening on fav icon click

--- frontend/src/components/Favourites.jsx
+++ frontend/src/components/Favourites.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import "../styles/Favourites.scss"; // Import the SCSS file
+import React from "react";
 
-const Favourites = ({ favouriteMovies, handleMovieClick }) => {
+const MovieItem = ({ movie, handleMovieClick, addToFavourites, removeFromFavourites, favouriteMovies = [] }) => {
+  const isFavourite = favouriteMovies.some(favMovie => favMovie.id === movie.id);
 
-    console.log("favouriteMovies:", favouriteMovies);
-    return (
-      <div className="favourites-container">
-        <div className="heading-container">
-          <h2 className="favourites-title">Favourites</h2>
-        </div>
-        <div className="favourites-list">
-          {favouriteMovies.map(movie => (
-            <div key={movie.id}
-              className="favourites-list-item"
-              onClick={() => handleMovieClick(movie)}
-              >
-              <h3>{movie.title}</h3>
-              <img src={movie.poster_url} alt={movie.title} className="favourites-image" />
-              <p>Genre: {movie.genre}</p>
-              <p>Release Year: {movie.release_year}</p>
-            </div>
-          ))}
-        </div>
+
+  const handleFavouritesClick = (e) => {
+    e.stopPropagation(); // Prevent the click from propagating to the movie item
+    if (isFavourite) {
+      console.log("Removing from favourites:", movie.title);
+      removeFromFavourites(movie);
+    } else {
+      console.log("Adding to favourites:", movie.title);
+      addToFavourites(movie);
+    }
+  };
+
+  return (
+    <div className="movie-list__item" onClick={() => handleMovieClick(movie)}>
+      <img src={movie.poster_url} alt={movie.title} />
+      <h2 className="movie-title">{movie.title}</h2>
+      <div className="movie-details">
+        <p>Genre: {movie.genre}</p>
+        <p>Release Year: {movie.release_year}</p>
+        <button
+          className="favourites_btn"
+          style={{ backgroundColor: isFavourite ? "red" : "yellow" }}
+          onClick={handleFavouritesClick}
+        >
+          ❤️
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
-export default Favourites;
+export default MovieItem;
