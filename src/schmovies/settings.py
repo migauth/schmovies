22eaabdded67606f0b36ad72c2ce5324486dba0a
Michael Gauthier <mikegauthier222@hotmail.com>
2025-05-22 11:49:03 -0700

    update allowed hosts

--- src/schmovies/settings.py
+++ src/schmovies/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
  
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'schmovies.onrender.com']
 
 
 # Application definition
